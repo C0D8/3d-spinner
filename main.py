d1474150bbe8d1e9ef0296ed7a2019a23b44f9dc
C0D8 <matheusaguiar362@gmail.com>
2023-04-03 10:52:37 -0300

    Mimentação no espaço sem o mouse ainda

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ vertices = vertices.T
 vertices = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
 vertices = vertices.T
 
-
 edges = [
     [0, 1],
     [1, 2],
@@ -35,20 +34,30 @@ edges = [
     [3, 7]
 ]
 
-
-
 pygame.init()
 
 # Defina as dimensões da janela
-width = 500
-height = 500
+width = 800
+height = 800
 position = [width // 2, height // 2]
 player = np.array([[0], [0], [1], [1]])
 move_status = {"left" : False, "rigth" : False, "front" : False, "back" : False}
 speed = 10
 
+zoom_in = np.array([
+    [1.05, 0, 0, 0],
+    [0, 1.05, 0, 0],
+    [0, 0, 1.05, 0],
+    [0, 0, 0, 1]
+])
 
 
+zoom_out = np.array([
+    [0.95, 0, 0, 0],
+    [0, 0.95, 0, 0],
+    [0, 0, 0.95, 0],
+    [0, 0, 0, 1]
+])
 
 
 # Crie uma janela
@@ -66,16 +75,20 @@ while running:
     transformed_vertices = []
 
     for vertex in vertices:
+
+
+
         x = rotation_x(tetha) @ rotation_z(tetha) @ rotation_x(tetha) @ vertex 
+        # Normaliza as coordenadas do vértice dividindo pela coordenada W
+        x /= x[3]
 
-        x[0] = x[0]+player[0,0]
-        x[1] = x[1]+player[1,0]
-        x[2] = x[2]+player[2,0]
-
-        transformed_vertices.append(x)
-
+        x[:3] = np.add(x[:3], player[:3, 0])
+        # Adiciona o vértice transformado na lista
+        transformed_vertices.append(x[:3])
+        
     # Desenhe as arestas do cubo na tela
     for edge in edges:
+
         start = transformed_vertices[edge[0]]
         end = transformed_vertices[edge[1]]
         pygame.draw.line(screen, (255, 255, 255), (start[0] + position[0], start[1] + position[1]), (end[0] + position[0], end[1] + position[1]), 2)
@@ -107,14 +120,10 @@ while running:
             if event.key == pygame.K_a:
                 move_status['left'] = False
 
-
         if move_status.get('front'):
-            player[2] -= speed
+            vertices = vertices @ zoom_in
         elif move_status.get('back'):
-            player[2] += speed
-
-
-
+            vertices = vertices @ zoom_out
         if move_status.get('left'):
             player = move(-speed,0,0) @ player
         elif move_status.get('rigth'):
@@ -122,7 +131,7 @@ while running:
 
 
     # Atualize a rotação do cubo
-    tetha += 0.01
+    tetha += 0.05
 
 # Encerre o Pygame
 pygame.quit()
