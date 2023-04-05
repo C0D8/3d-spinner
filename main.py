cc864953c57f134d505cebc536a58a620f96504b
C0D8 <matheusaguiar362@gmail.com>
2023-04-05 18:20:29 -0300

    Pronto com 4 unidade de direção

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ vertices = np.array([
 
 )  * TAMANHO_CUBO
 
-
 vertices = vertices.T
 vertices = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
 
@@ -47,14 +46,11 @@ position = [width // 2, height // 2]
 
 player = np.array([[0], [0], [1], [1]]).T
 
-player_cam = np.array([[0], [0], [1], [1]])
-
 move_status = {"left" : False, "rigth" : False, "front" : False, "back" : False}
-move_direction = {"left" : False, "rigth" : False, "front" : False, "back" : False}
+
 speed = 5
 
-fov = 200
-
+fov = 300
 
 # Crie uma janela
 screen = pygame.display.set_mode((width, height))
@@ -63,48 +59,19 @@ pygame.display.set_caption('Cubo 3D')
 tetha = 0
 running = True
 
-
-color1 = (0, 255, 0)
-color2 = (0, 128, 0)
-
 # Defina a posição e tamanho do retângulo do gradiente
 
 while running:
 
     # Desenhe um fundo preto
-    screen.fill((135, 206, 235))
+    screen.fill((0, 0, 0))
 
     # Transforme cada vértice do cubo usando a matriz de rotação
 
-
-
-
     x = move(0+player[0,0],0+player[0,1],200+player[0,2]) @  rotation_x(tetha) @ rotation_z(tetha) @ rotation_y(tetha) @ vertices 
-    # Normaliza as coordenadas do vértice dividindo pela coordenada W
-    # x /= x[3]
-
-
-    # x = x * player[2]
 
     # Adiciona o vértice transformado na lista
     transformed_vertices = vertices_2d(x,fov)
-
-
-        
-    
-    
-    # Atualize a tela
-
-    rect = pygame.Rect(0, height/2, width, height/2)
-
-    # Desenhe o gradiente na tela
-    for y in range(rect.top, rect.bottom):
-        # Calcule o fator de interpolação entre as cores
-        t = (y - rect.top) / rect.height
-        # Misture as duas cores usando o fator de interpolação
-        color = tuple(int(c1 * (1-t) + c2 * t) for c1, c2 in zip(color1, color2))
-        # Desenhe a linha horizontal com a cor interpolada
-        pygame.draw.line(screen, color, (rect.left, y), (rect.right, y))
 
     # Desenhe as arestas do cubo na tela
     for edge in edges:
@@ -113,15 +80,15 @@ while running:
         end = transformed_vertices[edge[1]]
         pygame.draw.line(screen, (255, 255, 255), (start[0] + position[0], start[1] + position[1]), (end[0] + position[0], end[1] + position[1]), 2)
 
-    
-
     pygame.display.update()
 
     # Trate eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.KEYDOWN :
+
             if event.key == pygame.K_w:
                 move_status['front'] = True
             if event.key == pygame.K_s:
@@ -130,16 +97,6 @@ while running:
                 move_status['rigth'] = True
             if event.key == pygame.K_a:
                 move_status['left'] = True
-
-            if event.key == pygame.K_UP:
-                move_direction['front'] = True
-            if event.key == pygame.K_DOWN:
-                move_direction['back'] = True
-            if event.key == pygame.K_RIGHT:
-                move_direction['rigth'] = True
-            if event.key == pygame.K_LEFT:
-                move_direction['left'] = True
-
 
         elif event.type == pygame.KEYUP:
            
@@ -152,30 +109,25 @@ while running:
             if event.key == pygame.K_a:
                 move_status['left'] = False
 
-            if event.key == pygame.K_UP:
-                move_direction['front'] = False
-            if event.key == pygame.K_DOWN:
-                move_direction['back'] = False
-            if event.key == pygame.K_RIGHT:
-                move_direction['rigth'] = False
-            if event.key == pygame.K_LEFT:
-                move_direction['left'] = False
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
+
             if event.button == 4:  # scroll up
+
                 if fov < 1000:
                     fov += 4
-            elif event.button == 5:  # scroll fovown
+                    
+            elif event.button == 5:  # scroll down
+
                 if fov > 4:
                     fov -= 4
 
 
         if move_status.get('front'):
-            if  player[0,2] > 20:
+
+            if  player[0,2] > 0:
                 player[0,2] -= speed
 
         elif move_status.get('back'):
-
             
             player[0,2] += speed
     
@@ -183,7 +135,6 @@ while running:
         if move_status.get('left'):
             
             player[0,0] -= speed
-
 
         elif move_status.get('rigth'):
             
